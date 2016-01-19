ac96b0df99649a4274c80b207b2b68c6bade5867
james1293 <james.da.4th@gmail.com>
2016-01-18 20:15:28 -0500

    now using google js api. works, one bus

--- www/js/index.js
+++ www/js/index.js
@@ -16,42 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        //  Looks like this is the stuff that actually runs on the event. --J
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        console.log("navigator.geolocation works well");
-        alert("V2 navigator.geolocation works well");
-
-    }
-};
-
+ 
+ 
 var alertCurrentPosition = function() {
 	alert("about to get position");
 	// onSuccess Callback
@@ -96,6 +62,155 @@ var alertCurrentPosition = function() {
 	
 
 
-} 
+}
+
+var gtimePeriod = 5000;
+
+var randomCounterThingy = 33.5;
+
+var latLong;
+
+var gmarker;
+
+var gmap;
+
+var grespFromMainSingleRequest={'lat':randomCounterThingy,'lng':-82,'timee':0};
+
+var refreshMapOLDusingIFRAME = function (){
+	var piece1 = "https://www.google.com/maps/embed/v1/place?q=";
+	//33.474497
+	var piece2 = "%2C%20";
+	//-82.022683
+	var piece3 = "&key=AIzaSyAgqF0QYWaQXDsGva0za_XCDJ7nfbf5d7c";
+	toWrite = piece1 + randomCounterThingy.toString() + piece2 + "-82.1" + piece3;
+	document.getElementById('googlemap').src = toWrite;
+}
+
+
+
+var moveMarker = function() {
+	gmarker.setPosition({'lat':randomCounterThingy,'lng':-82.01})
+	randomCounterThingy = randomCounterThingy + 0.01;
+}
+
+var refreshCurrentBusLocation = function () {
+	// DO AN AJAX CALL TO THE SERVER
+	loadXMLDoc();
+	// http://jamesda4th.pythonanywhere.com/busloconly
+	
+	//var cLoc = {'lat':33.5,'lng':-82.01};
+	//return cLoc;
+}
+
+
+// http://stackoverflow.com/questions/8567114/how-to-make-an-ajax-call-without-jquery
+function loadXMLDoc() {
+    var xmlhttp;
+
+    xmlhttp = new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if(xmlhttp.status == 200){
+               //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+               tempResp=JSON.parse(xmlhttp.responseText);
+               tempResp['lng']=tempResp['lon'];
+               grespFromMainSingleRequest = tempResp;
+               // alert(grespFromMainSingleRequest);
+               refreshMarker()
+               
+           }
+           else if(xmlhttp.status == 400) {
+              alert('There was an error 400')
+           }
+           else {
+              alert('something else other than 200 was returned')
+           }
+        }
+    }
+	
+    xmlhttp.open("GET", "http://jamesda4th.pythonanywhere.com/busloconly", true); //true => Async
+    xmlhttp.send();
+}
+
+var refreshMarker = function () {
+	var cLoc = grespFromMainSingleRequest;
+	//alert(cLoc.lat+","+cLoc.lng);
+	gmarker.setPosition({'lat':cLoc.lat,'lng':cLoc.lng});
+}
+
+
+var startMap = function () {
+	var cLoc = grespFromMainSingleRequest;
+	
+	// other way to specify lat and long
+	//var latLong = new google.maps.LatLng(randomCounterThingy,-82.022683);
+	
+	var mapOptions = {
+		center: cLoc,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+	gmarker = new google.maps.Marker({
+		position: cLoc,
+		title: 'Bus1'
+	});
+	
+	gmap = new google.maps.Map(document.getElementById("geolocation"), mapOptions);
+		
+	gmarker.setMap(gmap);
+}
+
+
+    
+// this might not be the cleanest way, but I'm still learning here.
+var doOtherStuffNowThatDeviceIsReady = function() {
+		
+	startMap();
+	setInterval(refreshCurrentBusLocation, gtimePeriod);
+	
+	
+	// console.log("navigator.geolocation works well");
+	// alert("V2 navigator.geolocation works well");
+	
+	//do a request for http://jamesda4th.pythonanywhere.com/busloconly
+}
+ 
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        doOtherStuffNowThatDeviceIsReady();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+        
+        
+};
+ 
 
 
